c8a06c16dc9a97e22dae62c04f80d03729a5bfa1
Felipe Martins <felipewmartins@gmail.com>
2014-05-29 22:53:09 -0300

    Mais alguns ajustes no crud de despesa

--- src/main/java/net/servehttp/bytecom/controller/DespesaController.java
+++ src/main/java/net/servehttp/bytecom/controller/DespesaController.java
@@ -10,6 +10,7 @@ import javax.inject.Named;
 
 import net.servehttp.bytecom.persistence.GenericoJPA;
 import net.servehttp.bytecom.persistence.entity.Despesa;
+import net.servehttp.bytecom.persistence.entity.Fornecedor;
 import net.servehttp.bytecom.util.AlertaUtil;
 
 @Named
@@ -18,8 +19,13 @@ public class DespesaController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private List<Despesa> listDespesa;
+	private List<Fornecedor> listFornecedor;
+	private int fornecedorId;
 	private Despesa despesaSelecionada;
 	private Despesa novaDespesa = new Despesa();
+	
+	@Inject
+	private Util util;
 	@Inject
 	private GenericoJPA genericoJPA;
 	
@@ -28,8 +34,42 @@ public class DespesaController implements Serializable {
 	@PostConstruct
 	public void load(){
 		listDespesa = genericoJPA.buscarTodos(Despesa.class);
+		setListFornecedor(genericoJPA.buscarTodos(Fornecedor.class));
+		getParameters();
 	}
 	
+	public void salvar(){
+		novaDespesa.setFornecedor(genericoJPA.buscarPorId(Fornecedor.class, fornecedorId));
+		genericoJPA.salvar(novaDespesa);
+		AlertaUtil.alerta("Despesa gravada com sucesso!");
+		load();
+		novaDespesa = new Despesa();
+	}
+	
+	public void atualizar(){
+		genericoJPA.atualizar(despesaSelecionada);
+	}
+	
+	public void remover(){
+		System.out.println("Removendo");
+		List<Despesa> despesas = genericoJPA.buscarTodos("select d from Despesa d where d.despesa.id = ?1", 
+				despesaSelecionada.getId(), Despesa.class);
+		if(despesas.isEmpty()){
+			genericoJPA.remover(despesaSelecionada);
+			load();
+			AlertaUtil.alerta("Removido com sucesso!");
+			
+		}
+	}
+	
+	private void getParameters(){
+		String despesaId = util.getParameters("despesaId");
+		if((despesaId != null) && (despesaId.isEmpty())){
+			despesaSelecionada = genericoJPA.buscarPorId(Despesa.class, Integer.parseInt(despesaId));
+		}
+		
+	}
+
 	public List<Despesa> getListDespesa() {
 		return listDespesa;
 	}
@@ -54,25 +94,23 @@ public class DespesaController implements Serializable {
 		this.novaDespesa = novaDespesa;
 	}
 
-	public void salvar(){
-		genericoJPA.salvar(novaDespesa);
-		AlertaUtil.alerta("Despesa gravada com sucesso!");
+	public List<Fornecedor> getListFornecedor() {
+		return listFornecedor;
+	}
+
+	public void setListFornecedor(List<Fornecedor> listFornecedor) {
+		this.listFornecedor = listFornecedor;
+	}
+
+	public int getFornecedorId() {
+		return fornecedorId;
+	}
+
+	public void setFornecedorId(int fornecedorId) {
+		this.fornecedorId = fornecedorId;
 	}
 	
-	public void atualizar(){
-		genericoJPA.atualizar(despesaSelecionada);
-	}
 	
-	public void remover(){
-		System.out.println("Removendo");
-		List<Despesa> despesas = genericoJPA.buscarTodos("select d from Despesa d where d.despesa.id = ?1", 
-				despesaSelecionada.getId(), Despesa.class);
-		if(despesas.isEmpty()){
-			genericoJPA.remover(despesaSelecionada);
-			load();
-			AlertaUtil.alerta("Removido com sucesso!");
-			
-		}
-	}
+
 	
 }
