53f12f1e6e5c856f980f0885bd102b4ee540a74f
clairtonluz <clairton.c.l@gmail.com>
2014-06-19 11:13:24 -0300

    ajuste

---
+++ src/main/java/net/servehttp/bytecom/controller/ServidorController.java
@@ -0,0 +1,35 @@
+package net.servehttp.bytecom.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.Schedule;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class ServidorController {
+
+  private static final Logger logger = Logger.getLogger(ServidorController.class.getName());
+
+  public void atualizarAcesso() {
+    logger.info("\n######################################################"
+        + "\n#                                                    #"
+        + "\n#         ATUALIZANDO ACESSO DOS CLIENTES            #"
+        + "\n#                                                    #"
+        + "\n######################################################");
+
+    executar("/opt/script/./startServer.sh start");
+  }
+
+  private void executar(String command) {
+    try {
+      Runtime.getRuntime().exec(command);
+    } catch (IOException e) {
+      logger.info("O ACESSO DOS CLIENTES NÃO FOI ATUALIZADO PORQUE VOCÊ NÃO ESTA NO SERVIDOR, DEVIDO A ISSO VOCÊ NÃO TEM O SCRIPT DE ATUALIZAÇÃO /opt/script/startServer.sh");
+    }
+  }
+
+}
