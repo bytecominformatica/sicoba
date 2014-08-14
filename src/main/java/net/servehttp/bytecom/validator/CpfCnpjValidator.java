65d7034164bc0d841aeb100aa2f0e212ca4b38c2
clairtonluz <clairton.c.l@gmail.com>
2014-08-13 21:45:29 -0300

    refatorando annotations

--- src/main/java/net/servehttp/bytecom/validator/CpfCnpjValidator.java
+++ src/main/java/net/servehttp/bytecom/validator/CpfCnpjValidator.java
@@ -1,28 +1,34 @@
 package net.servehttp.bytecom.validator;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import net.servehttp.bytecom.annotation.CpfCnpj;
- 
- 
+
+
 /**
  * 
  * @author Clairton Luz - clairton.c.l@gmail.com
  *
  */
 public class CpfCnpjValidator implements ConstraintValidator<CpfCnpj, String> {
- 
+
+  private List<String> cnpjInvalido = Arrays.asList("00000000000000", "11111111111111",
+      "22222222222222", "33333333333333", "44444444444444", "55555555555555", "66666666666666",
+      "77777777777777", "88888888888888", "99999999999999");
+
   @Override
   public void initialize(CpfCnpj constraintAnnotation) {}
- 
+
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
     return value == null || value.isEmpty() || isCpf(value) || isCnpj(value);
   }
- 
+
   /**
    * Realiza a validação do CPF.
    * 
@@ -33,66 +39,66 @@ public class CpfCnpjValidator implements ConstraintValidator<CpfCnpj, String> {
   private boolean isCpf(String cpf) {
     cpf = cpf.replace(".", "");
     cpf = cpf.replace("-", "");
-    
-    try{
+
+    try {
       Long.parseLong(cpf);
-    } catch(NumberFormatException e){
+    } catch (NumberFormatException e) {
       return false;
     }
- 
+
     int d1, d2;
     int digito1, digito2, resto;
     int digitoCPF;
     String nDigResult;
- 
+
     d1 = d2 = 0;
     digito1 = digito2 = resto = 0;
- 
+
     for (int nCount = 1; nCount < cpf.length() - 1; nCount++) {
       digitoCPF = Integer.valueOf(cpf.substring(nCount - 1, nCount)).intValue();
- 
+
       // multiplique a ultima casa por 2 a seguinte por 3 a seguinte por 4
       // e assim por diante.
       d1 = d1 + (11 - nCount) * digitoCPF;
- 
+
       // para o segundo digito repita o procedimento incluindo o primeiro
       // digito calculado no passo anterior.
       d2 = d2 + (12 - nCount) * digitoCPF;
     };
- 
+
     // Primeiro resto da divisão por 11.
     resto = (d1 % 11);
- 
+
     // Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11
     // menos o resultado anterior.
     if (resto < 2)
       digito1 = 0;
     else
       digito1 = 11 - resto;
- 
+
     d2 += 2 * digito1;
- 
+
     // Segundo resto da divisão por 11.
     resto = (d2 % 11);
- 
+
     // Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11
     // menos o resultado anterior.
     if (resto < 2)
       digito2 = 0;
     else
       digito2 = 11 - resto;
- 
+
     // Digito verificador do CPF que está sendo validado.
     String nDigVerific = cpf.substring(cpf.length() - 2, cpf.length());
- 
+
     // Concatenando o primeiro resto com o segundo.
     nDigResult = String.valueOf(digito1) + String.valueOf(digito2);
- 
+
     // comparar o digito verificador do cpf com o primeiro resto + o segundo
     // resto.
     return nDigVerific.equals(nDigResult);
   }
- 
+
   /**
    * Realiza a validação de um cnpj
    * 
@@ -103,19 +109,15 @@ public class CpfCnpjValidator implements ConstraintValidator<CpfCnpj, String> {
     cnpj = cnpj.replace(".", "");
     cnpj = cnpj.replace("-", "");
     cnpj = cnpj.replace("/", "");
-    
-    try{
+
+    try {
       Long.parseLong(cnpj);
-    } catch(NumberFormatException e){
+    } catch (NumberFormatException e) {
       return false;
     }
-    
+
     // considera-se erro CNPJ's formados por uma sequencia de numeros iguais
-    if (cnpj.equals("00000000000000") || cnpj.equals("11111111111111")
-        || cnpj.equals("22222222222222") || cnpj.equals("33333333333333")
-        || cnpj.equals("44444444444444") || cnpj.equals("55555555555555")
-        || cnpj.equals("66666666666666") || cnpj.equals("77777777777777")
-        || cnpj.equals("88888888888888") || cnpj.equals("99999999999999") || (cnpj.length() != 14))
+    if (cnpjInvalido.contains(cnpj) || (cnpj.length() != 14))
       return (false);
     char dig13, dig14;
     int sm, i, r, num, peso; // "try" - protege o código para eventuais
@@ -134,13 +136,13 @@ public class CpfCnpjValidator implements ConstraintValidator<CpfCnpj, String> {
         if (peso == 10)
           peso = 2;
       }
- 
+
       r = sm % 11;
       if ((r == 0) || (r == 1))
         dig13 = '0';
       else
         dig13 = (char) ((11 - r) + 48);
- 
+
       // Calculo do 2o. Digito Verificador
       sm = 0;
       peso = 2;
