63b7e55a2621844045f5825fe6d562587fc478d3
felipewmartins <felipewmartins@gmail.com>
2014-09-03 18:37:21 -0300

    Mapas

---
+++ src/main/webapp/js/maps/map.js
@@ -0,0 +1,23 @@
+/**
+ * Script de carregamento do mapa padrão.
+ */
+function initialize(){
+	var mapOptions = {
+			zoom: 8,
+			center: new google.maps.LatLng(-34.397, 150.644)
+			
+	}
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+		      mapOptions);
+
+}
+
+function loadScript() {
+	  var script = document.createElement('script');
+	  script.type = 'text/javascript';
+	  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+	      'callback=initialize';
+	  document.body.appendChild(script);
+	}
+
+	window.onload = loadScript;
