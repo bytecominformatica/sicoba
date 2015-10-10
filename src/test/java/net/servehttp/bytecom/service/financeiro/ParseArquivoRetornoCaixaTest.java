ba9fad0894c777995c33372eba170f074a9755c0
clairtonluz <clairton.c.l@gmail.com>
2015-10-10 08:42:49 -0300

    implementando teste para o tratamento do arquivo de retorno

--- src/test/java/net/servehttp/bytecom/service/financeiro/ParseArquivoRetornoCaixaTest.java
+++ src/test/java/net/servehttp/bytecom/service/financeiro/ParseArquivoRetornoCaixaTest.java
@@ -1,17 +1,19 @@
 package net.servehttp.bytecom.service.financeiro;
 
-import static org.junit.Assert.*;
-
 import net.servehttp.bytecom.persistence.jpa.entity.financeiro.retorno.Header;
 import net.servehttp.bytecom.persistence.jpa.entity.financeiro.retorno.HeaderLote;
 import net.servehttp.bytecom.persistence.jpa.entity.financeiro.retorno.Registro;
+import net.servehttp.bytecom.persistence.jpa.entity.financeiro.retorno.RegistroDetalhe;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by clairton on 09/10/15.
@@ -19,29 +21,48 @@ import java.time.format.DateTimeFormatter;
 public class ParseArquivoRetornoCaixaTest {
 
     private ParseArquivoRetornoCaixa parseArquivoRetornoCaixa = new ParseArquivoRetornoCaixa();
+    private Header header;
     private static final double DELTA = 0d;
 
-    @Test
-    public void deveriaTratarOArquivoDeRetornoCaixa() throws IOException {
+    @Before
+    public void beforeEach() throws IOException {
         InputStream input = getClass().getResourceAsStream("/files/ret000149.ret");
         assertNotNull(input);
+        header = parseArquivoRetornoCaixa.parse(input, "ret000149.ret");
+    }
 
-        Header h = parseArquivoRetornoCaixa.parse(input, "ret000149.ret");
+    @Test
+    public void deveriaVerificarTratamentoDoHeader() throws IOException {
+        assertEquals("ret000149.ret", header.getNomeArquivo());
+        assertEquals(LocalDateTime.of(2015, 2, 19, 1, 0, 46), header.getDataGeracao());
+        assertEquals(149, header.getSequencial());
+        assertEquals(1, header.getHeaderLotes().size());
+        assertEquals(1, header.getTrailer().getQuantidadeLotes());
+        assertEquals(14, header.getTrailer().getQuantidadeRegistros());
+    }
 
-        assertNotNull(h);
-        assertEquals("ret000149.ret", h.getNomeArquivo());
-        assertEquals(LocalDateTime.of(2015, 2, 19, 1, 0, 46), h.getDataGeracao());
-        assertEquals(149, h.getSequencial());
-        assertEquals(1, h.getHeaderLotes().size());
-        assertEquals(1, h.getTrailer().getQuantidadeLotes());
-        assertEquals(14, h.getTrailer().getQuantidadeRegistros());
+    @Test
+    public void deveriaVerificarTratamentoDoTrailer() throws IOException {
+        assertEquals(1, header.getTrailer().getQuantidadeLotes());
+        assertEquals(14, header.getTrailer().getQuantidadeRegistros());
+    }
 
-        HeaderLote headerLote = h.getHeaderLotes().get(0);
+    @Test
+    public void deveriaVerificarTratamentoDoHeaderLote() throws IOException {
+        HeaderLote headerLote = header.getHeaderLotes().get(0);
         assertEquals(149, headerLote.getNumeroRemessaRetorno());
         assertEquals(LocalDate.of(2015, 2, 19), headerLote.getDataGravacaoRemessaRetorno());
-        assertEquals(5, headerLote.getRegistros().size());
+    }
+    @Test
+    public void deveriaVerificarTratamentoDoTrailerLote() throws IOException {
+        HeaderLote headerLote = header.getHeaderLotes().get(0);
         assertEquals(12, headerLote.getTrailerLote().getQuantidadeRegistroLote());
+    }
 
+    @Test
+    public void deveriaVerificarTratamentoDoRegistro() throws IOException {
+        HeaderLote headerLote = header.getHeaderLotes().get(0);
+        assertEquals(5, headerLote.getRegistros().size());
         Registro registro = headerLote.getRegistros().get(0);
 
         assertEquals(24, registro.getModalidadeNossoNumero());
@@ -59,6 +80,21 @@ public class ParseArquivoRetornoCaixaTest {
         assertEquals(0d, registro.getRegistroDetalhe().getJurosMultasEncargos(), DELTA);
         assertEquals(35d, registro.getRegistroDetalhe().getValorLiquido(), DELTA);
         assertEquals(35d, registro.getRegistroDetalhe().getValorPago(), DELTA);
+    }
+    @Test
+    public void deveriaVerificarTratamentoDoRegistroDetalhe() throws IOException {
+        HeaderLote headerLote = header.getHeaderLotes().get(0);
+        RegistroDetalhe registroDetalhe = headerLote.getRegistros().get(0).getRegistroDetalhe();
+
+        assertEquals(0d, registroDetalhe.getAbatimento(), DELTA);
+        assertEquals(LocalDate.of(2015, 2, 20), registroDetalhe.getDataCredito());
+        assertEquals(LocalDate.of(2015, 2, 20), registroDetalhe.getDataDebitoTarifa());
+        assertEquals(LocalDate.of(2015, 2, 19), registroDetalhe.getDataOcorrencia());
+        assertEquals(0d, registroDetalhe.getDesconto(), DELTA);
+        assertEquals(0d, registroDetalhe.getIof(), DELTA);
+        assertEquals(0d, registroDetalhe.getJurosMultasEncargos(), DELTA);
+        assertEquals(35d, registroDetalhe.getValorLiquido(), DELTA);
+        assertEquals(35d, registroDetalhe.getValorPago(), DELTA);
     }
 
 }
