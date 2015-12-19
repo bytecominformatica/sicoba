aba08a9b6e8c14499389e4b332422a59e78ef9ea
clairtonluz <clairton.c.l@gmail.com>
2015-12-19 15:10:45 -0300

    exibindo clientes sem mensalidade

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,11 +1,23 @@
 'use strict';
 
 angular.module('sicobaApp')
-    .controller('HomeCtrl', function ($scope, Mensalidade) {
+    .controller('HomeCtrl', function ($scope, Mensalidade, Cliente) {
+
+        $scope.order = _order;
 
         _buscarMensalidadesAtrasada();
+        _buscarClientesSemMensalidade();
 
         function _buscarMensalidadesAtrasada() {
             $scope.mensalidadesAtrasada = Mensalidade.atrasados();
+        }
+
+        function _buscarClientesSemMensalidade() {
+            $scope.clientesSemMensalidade = Cliente.semMensalidade();
+        }
+
+        function _order(predicate) {
+            $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+            $scope.predicate = predicate;
         }
     });
--- src/main/webapp/app/service/comercial/cliente.service.js
+++ src/main/webapp/app/service/comercial/cliente.service.js
@@ -1,3 +1,16 @@
 /**
  * Created by clairton on 19/12/15.
  */
+'use strict';
+
+angular.module('sicobaApp')
+    .factory('Cliente', ['$resource', function($resource) {
+        return $resource('/clientes/:id', {id: '@id'},
+            {
+                'semMensalidade': {
+                    method:'GET',
+                    url: 'api/clientes/sem_mensalidade',
+                    isArray: true
+                }
+            });
+    }]);
