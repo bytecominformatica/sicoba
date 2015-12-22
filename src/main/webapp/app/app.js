87e7ed8425c1ed8c5e94c154b62b4a332b039604
clairtonluz <clairton.c.l@gmail.com>
2015-12-21 22:19:30 -0300

    implementando crud de cliente

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -4,6 +4,7 @@ angular.module('sicobaApp', [
     'ngRoute',
     'ngResource',
     'ngAnimate',
+    'ui.bootstrap',
     'angular-loading-bar'
 ]).
 config(['$routeProvider', function ($routeProvider) {
--- src/main/webapp/app/controllers/cliente/cliente.controller.js
+++ src/main/webapp/app/controllers/cliente/cliente.controller.js
@@ -1,7 +1,22 @@
 'use strict';
 
 angular.module('sicobaApp')
-    .controller('ClienteCtrl', function ($scope, Cliente) {
-        $scope.cliente = {status: 'ATIVO'}
+    .controller('ClienteCtrl', function ($scope, $routeParams, Cliente) {
 
+        $scope.save = _save;
+
+        _init();
+
+        function _init() {
+            $scope.cliente = {status: 'ATIVO'};
+            $scope.hoje = new Date();
+
+            if ($routeParams.id) {
+                $scope.cliente = Cliente.get({id: $routeParams.id});
+            }
+        }
+
+        function _save(cliente) {
+            Cliente.save(cliente);
+        }
     });
