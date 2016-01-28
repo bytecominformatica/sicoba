f5fca3a94bf83b8d815ebd5ea350f587671ff2b8
clairtonluz <clairton.c.l@gmail.com>
2016-01-28 20:53:36 -0300

    ajustes

---
+++ src/main/java/br/com/clairtonluz/sicoba/config/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package br.com.clairtonluz.sicoba.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by clairtonluz on 28/01/16.
+ */
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
