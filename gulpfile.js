a1e6f2042148ce803eef2f3b123ef256e5aadff0
clairtonluz <clairton.c.l@gmail.com>
2016-02-27 10:39:26 -0300

    ajuste

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,25 @@ gulp.task('default', ['watch']);
 
 // configura a tarefa jshint
 gulp.task('jshint', function () {
-    return gulp.src('public/app/**/*.js')
+    return gulp.src('src/main/resources/public/app/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // configura quais arquivos monitorar e qual tarefa chamar quando ouver alterações neles
 gulp.task('watch', function () {
-    gulp.watch('public/app/**/*.js', ['build-js']);
+    gulp.watch('src/main/resources/public/app/**/*.js', ['build-js']);
 });
 
 /** configura quais arquivos serao concatenados e minifica esses arquivos caso encontre
  * a propriedade --type production e escreve tudo isso no arquivo all.js
  * */
 gulp.task('build-js', ['jshint'], function() {
-    return gulp.src('public/app/**/*.js')
+    return gulp.src('src/main/resources/public/app/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
         //only uglify if gulp is ran with '--type production'
         .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/dist/js'));
+        .pipe(gulp.dest('src/main/resources/public/dist/js'));
 });
