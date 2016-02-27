a9e1a3b5c672c96903596e5ad207d5cfb9fcd2c2
clairtonluz <clairton.c.l@gmail.com>
2016-02-27 10:49:04 -0300

    ajustes

--- src/main/resources/public/app/componentes/http.interceptor.js
+++ src/main/resources/public/app/componentes/http.interceptor.js
@@ -7,45 +7,42 @@
     angular.module('sicobaApp')
         .factory('HttpInterceptor', function ($rootScope, $location, $cookies) {
 
-            var isRequestServerSide = _isRequestServerSide;
-            var isHtml = _isHtml;
-            var isLoginPage = _isLoginPage;
-            var isMenu = _isMenu;
+            //var isRequestServerSide = _isRequestServerSide;
+            //var isHtml = _isHtml;
+            //var isLoginPage = _isLoginPage;
+            //var isMenu = _isMenu;
 
             return {
                 request: function (config) {
-                    if (isRequestServerSide(config.url)) {
-                        config.url = 'http://localhost:5000/' + config.url;
-                        config.headers['X-XSRF-TOKEN'] = $cookies.get('XSRF-TOKEN');
-                    }
+                    //if (isRequestServerSide(config.url)) {
+                    //    config.url = 'http://localhost:5000/' + config.url;
+                    //    config.headers['X-XSRF-TOKEN'] = $cookies.get('XSRF-TOKEN');
+                    //}
 
                     return config;
                 },
                 responseError: function (rejection) {
                     if (rejection.status === 401) {
-                        $rootScope.authenticated = false;
-                        $rootScope.pageId = 'page-not-logged';
                         $location.path('/login');
                     }
                 }
             };
 
-
-            function _isRequestServerSide(url) {
-                return !(isHtml(url) || isMenu(url));
-            }
-
-            function _isHtml(url) {
-                return url.indexOf('.html') > -1;
-            }
-
-            function _isLoginPage(url) {
-                return url.indexOf('/login') > -1;
-            }
-
-            function _isMenu(url) {
-                return url.indexOf('menu.json') > -1;
-            }
+            //function _isRequestServerSide(url) {
+            //    return !(isHtml(url) || isMenu(url));
+            //}
+            //
+            //function _isHtml(url) {
+            //    return url.indexOf('.html') > -1;
+            //}
+            //
+            //function _isLoginPage(url) {
+            //    return url.indexOf('/login') > -1;
+            //}
+            //
+            //function _isMenu(url) {
+            //    return url.indexOf('menu.json') > -1;
+            //}
         })
         .config(function ($httpProvider) {
             $httpProvider.interceptors.push('HttpInterceptor');
