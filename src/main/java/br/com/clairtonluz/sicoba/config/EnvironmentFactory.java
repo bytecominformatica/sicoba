93e24547a37b0675b5b7afd76b78fcd7600ca798
clairtonluz <clairton.c.l@gmail.com>
2016-12-23 23:59:49 -0300

    Criando ambiente de qualidade

--- src/main/java/br/com/clairtonluz/sicoba/config/EnvironmentFactory.java
+++ src/main/java/br/com/clairtonluz/sicoba/config/EnvironmentFactory.java
@@ -2,6 +2,7 @@ package br.com.clairtonluz.sicoba.config;
 
 import br.com.clairtonluz.sicoba.config.impl.EnvironmentDevelopment;
 import br.com.clairtonluz.sicoba.config.impl.EnvironmentProduction;
+import br.com.clairtonluz.sicoba.config.impl.EnvironmentQuality;
 
 /**
  * Created by clairtonluz on 28/02/16.
@@ -10,18 +11,28 @@ public class EnvironmentFactory {
 
     private static final Environment PRODUCTION;
     private static final Environment DEVELOPMENT;
+    private static final Environment QUALITY;
 
     static {
         PRODUCTION = new EnvironmentProduction();
         DEVELOPMENT = new EnvironmentDevelopment();
+        QUALITY = new EnvironmentQuality();
     }
 
     public static Environment create() {
         String env = System.getenv("ENV");
-        if (env != null && env.equals(Environment.PRODUCTION)) {
-            return PRODUCTION;
-        } else {
-            return DEVELOPMENT;
+        Environment current;
+        switch (env) {
+            case Environment.PRODUCTION:
+                current = PRODUCTION;
+                break;
+            case Environment.QUALITY:
+                current = QUALITY;
+                break;
+            default:
+                current = DEVELOPMENT;
         }
+
+        return current;
     }
 }
--- src/main/java/br/com/clairtonluz/sicoba/config/impl/EnvironmentQuality.java
+++ src/main/java/br/com/clairtonluz/sicoba/config/impl/EnvironmentQuality.java
@@ -5,9 +5,9 @@ import br.com.clairtonluz.sicoba.config.Environment;
 /**
  * Created by clairtonluz on 28/02/16.
  */
-public class EnvironmentDevelopment implements Environment {
+public class EnvironmentQuality implements Environment {
     @Override
     public String getEnv() {
-        return Environment.DEVELOPMENT;
+        return Environment.QUALITY;
     }
 }
