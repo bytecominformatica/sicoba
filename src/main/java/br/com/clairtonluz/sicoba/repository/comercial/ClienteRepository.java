48162cd9d05a5d5c163048bc163856e56ef47bf0
clairtonluz <clairton.c.l@gmail.com>
2016-12-24 16:28:32 -0300

    ajustando listagem de clientes sem titulo para verificar também se existe cobranças

--- src/main/java/br/com/clairtonluz/sicoba/repository/comercial/ClienteRepository.java
+++ src/main/java/br/com/clairtonluz/sicoba/repository/comercial/ClienteRepository.java
@@ -20,11 +20,11 @@ public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
     String QUERY_CLIENTE_SEM_TITULOS = "select c from Cliente c where c.status <> 2 " +
             "and (" +
             "(SELECT count(*) FROM Titulo t where t.cliente.id = c.id and t.dataVencimento > :date) " +
-            "+ (SELECT count(*) FROM Charge t where t.cliente.id = c.id and t.expireAt > :date)" +
+            "+ (SELECT count(*) FROM Charge t where t.cliente.id = c.id and t.status <> 'NEW' and t.expireAt > :date)" +
             ") < 2 " +
             "order by ( " +
             "(SELECT count(*) FROM Titulo t where t.cliente.id = c.id and t.dataVencimento > :date) + " +
-            "(SELECT count(*) FROM Charge t where t.cliente.id = c.id and t.expireAt > :date) " +
+            "(SELECT count(*) FROM Charge t where t.cliente.id = c.id and t.status <> 'NEW' and t.expireAt > :date) " +
             ")";
 
     List<Cliente> findByStatus(StatusCliente status);
