eb46c0512e94d27ef86387f1554751ff718b7e64
clairtonluz <clairton.c.l@gmail.com>
2017-07-15 13:18:50 -0300

    correção nas permissões da url de notificação gerencianet

--- src/main/java/br/com/clairtonluz/sicoba/api/financeiro/gerencianet/NotificationAPI.java
+++ src/main/java/br/com/clairtonluz/sicoba/api/financeiro/gerencianet/NotificationAPI.java
@@ -4,6 +4,8 @@ import br.com.clairtonluz.sicoba.service.financeiro.gerencianet.notification.Not
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 /**
  * Created by clairtonluz on 19/12/15.
  */
@@ -18,6 +20,28 @@ public class NotificationAPI {
     public void process(@PathVariable Integer accountId, @RequestBody String body) {
         String token = body.substring(body.indexOf('=') + 1);
         notificationService.processNotification(accountId, token);
+    }
+
+    @RequestMapping(value = "/{accountId}/lote/notification", method = RequestMethod.POST)
+    public Map process(@PathVariable Integer accountId, @RequestBody List<String> tokenRecebidos) {
+        Map map = new HashMap<String, String>();
+        Set<String> tokens = new HashSet<>(tokenRecebidos);
+        int count = 0;
+        List<String> falhas = new ArrayList<>();
+        for (String token : tokens) {
+            try {
+                notificationService.processNotification(accountId, token);
+                count++;
+            } catch (Exception e) {
+                falhas.add(token + ":" + e.getLocalizedMessage());
+            }
+        }
+        map.put("recebidos", String.valueOf(tokenRecebidos.size()));
+        map.put("distintos", String.valueOf(tokens.size()));
+        map.put("processados", String.valueOf(count));
+        map.put("falhas", falhas.toString());
+
+        return map;
     }
 
 }
