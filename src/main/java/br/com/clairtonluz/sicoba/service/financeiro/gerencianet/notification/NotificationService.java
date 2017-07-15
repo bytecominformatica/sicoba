ed17eb02243b34fb1c91d5ba9af068aad1c2e7b4
clairtonluz <clairton.c.l@gmail.com>
2017-07-15 13:55:28 -0300

    correção da data de pagamento

--- src/main/java/br/com/clairtonluz/sicoba/service/financeiro/gerencianet/notification/NotificationService.java
+++ src/main/java/br/com/clairtonluz/sicoba/service/financeiro/gerencianet/notification/NotificationService.java
@@ -12,6 +12,7 @@ import br.com.clairtonluz.sicoba.repository.financeiro.gerencianet.ChargeReposit
 import br.com.clairtonluz.sicoba.repository.financeiro.gerencianet.GerencianetAccountRepository;
 import br.com.clairtonluz.sicoba.service.comercial.ClienteService;
 import br.com.clairtonluz.sicoba.service.financeiro.gerencianet.GNService;
+import br.com.clairtonluz.sicoba.util.DateUtil;
 import br.com.clairtonluz.sicoba.util.SendEmail;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
@@ -57,7 +58,7 @@ public class NotificationService {
                             processarCarnet(token, it);
                             break;
                         case "carnet_charge":
-                            processarCarnetCharge(token, it);
+                            processarCharge(token, it);
                             break;
                         default:
                             Logger.getLogger(getClass().getName()).warning(it.toString());
@@ -68,53 +69,6 @@ public class NotificationService {
             }
         } else {
             SendEmail.sendToAdmin("[NOTIFICATION] Conta gerencianet não encontrada", String.format("conta gerencianet:%s\ntoken:%s", gerencianetAccountId, token));
-        }
-    }
-
-    private void processarCharge(String token, JSONObject data) {
-        Charge charge = chargeRepository.findOptionalByChargeId(data.getJSONObject("identifiers").getInt("charge_id"));
-        if (charge == null) {
-            SendEmail.sendToAdmin("[NOTIFICATION] Cobrança não processada", "Cobrança não processado:" + data.toString());
-            return;
-        }
-
-        int id = data.getInt("id");
-        if (charge.getLastNotification() == null || id > charge.getLastNotification()) {
-            charge.setTokenNotification(token);
-            charge.setLastNotification(id);
-            JSONObject status = data.getJSONObject("status");
-            charge.setStatus(StatusCharge.valueOf(status.getString("current").toUpperCase()));
-
-            if (charge.getStatus().equals(StatusCharge.PAID)) {
-                charge.setPaidValue(data.getDouble("value") / 100);
-                charge.setPaidAt(new Date());
-
-                Cliente cliente = charge.getCliente();
-                String messageVerify = charge.verifyPayment();
-                if (!messageVerify.equals(Charge.VALID_PAYMENT)) {
-                    try {
-                        clienteService.inativar(cliente);
-                        String subject = String.format("Cliente %s bloqueado por pagamento inválido", cliente.getNome());
-                        String chargeJson = new ObjectMapper().writeValueAsString(charge);
-                        String message = String.format("Cobrana: %s\nMotivo:%s", chargeJson, messageVerify);
-                        SendEmail.sendToAdmin(subject, message);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        String subject = String.format("Não foi possível bloquear o cliente %s por pagamento inválido do tipo:%s", cliente.getNome(), messageVerify);
-                        SendEmail.notificarAdmin(subject, e);
-                    }
-                } else if (cliente.getStatus().equals(StatusCliente.INATIVO)) {
-                    try {
-                        clienteService.ativar(cliente);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        String subject = String.format("Não foi possível ativar o cliente %s", cliente.getNome());
-                        SendEmail.notificarAdmin(subject, e);
-                    }
-                }
-            }
-
-            chargeRepository.save(charge);
         }
     }
 
@@ -136,10 +90,10 @@ public class NotificationService {
         }
     }
 
-    private void processarCarnetCharge(String token, JSONObject data) {
+    private void processarCharge(String token, JSONObject data) {
         Charge charge = chargeRepository.findOptionalByChargeId(data.getJSONObject("identifiers").getInt("charge_id"));
         if (charge == null) {
-            SendEmail.sendToAdmin("[NOTIFICATION]Parcela do carnê não processado", "Parcela carnê não processado:" + data.toString());
+            SendEmail.sendToAdmin("[NOTIFICATION] Cobrança não processada", "Cobrança não processado:" + data.toString());
             return;
         }
 
@@ -152,11 +106,44 @@ public class NotificationService {
 
             if (charge.getStatus().equals(StatusCharge.PAID)) {
                 charge.setPaidValue(data.getDouble("value") / 100);
-                charge.setPaidAt(new Date());
+                Date paidAt = getCreatedAt(data);
+                charge.setPaidAt(paidAt);
+                ativarCliente(charge);
             }
 
             chargeRepository.save(charge);
         }
+    }
+
+    private void ativarCliente(Charge charge) {
+        Cliente cliente = charge.getCliente();
+        String messageVerify = charge.verifyPayment();
+        if (!messageVerify.equals(Charge.VALID_PAYMENT)) {
+            try {
+                clienteService.inativar(cliente);
+                String subject = String.format("Cliente %s bloqueado por pagamento inválido", cliente.getNome());
+                String chargeJson = new ObjectMapper().writeValueAsString(charge);
+                String message = String.format("Cobrana: %s\nMotivo:%s", chargeJson, messageVerify);
+                SendEmail.sendToAdmin(subject, message);
+            } catch (Exception e) {
+                e.printStackTrace();
+                String subject = String.format("Não foi possível bloquear o cliente %s por pagamento inválido do tipo:%s", cliente.getNome(), messageVerify);
+                SendEmail.notificarAdmin(subject, e);
+            }
+        } else if (cliente.getStatus().equals(StatusCliente.INATIVO)) {
+            try {
+                clienteService.ativar(cliente);
+            } catch (Exception e) {
+                e.printStackTrace();
+                String subject = String.format("Não foi possível ativar o cliente %s", cliente.getNome());
+                SendEmail.notificarAdmin(subject, e);
+            }
+        }
+    }
+
+
+    private Date getCreatedAt(JSONObject data) {
+        return DateUtil.parseDateISO(data.getString("created_at"));
     }
 
 }
