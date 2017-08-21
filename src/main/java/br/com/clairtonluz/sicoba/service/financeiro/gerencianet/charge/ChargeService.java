09795dc7d8cda7c53a41d85c9da41c5d8e66b3b4
clairtonluz <clairton.c.l@gmail.com>
2017-08-21 14:08:37 -0300

    'ajuste'

--- src/main/java/br/com/clairtonluz/sicoba/service/financeiro/gerencianet/charge/ChargeService.java
+++ src/main/java/br/com/clairtonluz/sicoba/service/financeiro/gerencianet/charge/ChargeService.java
@@ -10,7 +10,7 @@ import br.com.clairtonluz.sicoba.repository.comercial.ClienteRepository;
 import br.com.clairtonluz.sicoba.repository.comercial.ContratoRepository;
 import br.com.clairtonluz.sicoba.repository.financeiro.gerencianet.ChargeRepository;
 import br.com.clairtonluz.sicoba.service.financeiro.gerencianet.GNService;
-import br.com.clairtonluz.sicoba.service.financeiro.gerencianet.carnet.CarnetGNService;
+import br.com.clairtonluz.sicoba.service.financeiro.gerencianet.carnet.CarnetService;
 import br.com.clairtonluz.sicoba.util.DateUtil;
 import br.com.clairtonluz.sicoba.util.SendEmail;
 import br.com.clairtonluz.sicoba.util.StringUtil;
@@ -39,7 +39,7 @@ public class ChargeService {
     @Autowired
     private ChargeGNService chargeGNService;
     @Autowired
-    private CarnetGNService carnetGNService;
+    private CarnetService carnetService;
 
     @Transactional
     public Charge createCharge(Charge charge) {
@@ -106,13 +106,13 @@ public class ChargeService {
                 StringUtil.formatCurrence(chargeAtual.getValue()), StringUtil.formatCurrence(chargeAtual.getPaidValue()));
 
         if (chargeAtual.getCarnet() == null) {
-            chargeAtual = cancelCharge(chargeAtual);
+            cancelCharge(chargeAtual);
         } else {
-            chargeAtual = carnetGNService.cancelParcel(chargeAtual);
+            carnetService.cancelParcel(chargeAtual);
         }
 
         SendEmail.sendToAdmin(subject, content);
-        return chargeAtual; // cancela o boleto no integrador e salva a cobrança com as alterações
+        return chargeAtual;
     }
 
     @Transactional
