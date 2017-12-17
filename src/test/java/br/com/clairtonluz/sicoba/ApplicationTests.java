0f90f67ff337d0bf5318ea5e80ca4d15a811cb84
clairtonluz <clairton.c.l@gmail.com>
2017-12-17 19:53:54 -0300

    correção dos testes

--- src/test/java/br/com/clairtonluz/sicoba/ApplicationTests.java
+++ src/test/java/br/com/clairtonluz/sicoba/ApplicationTests.java
@@ -2,16 +2,16 @@ package br.com.clairtonluz.sicoba;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
+    @Test
+    public void contextLoads() {
 
-	}
+    }
 
 }
