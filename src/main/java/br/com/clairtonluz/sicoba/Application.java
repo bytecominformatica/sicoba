15dfc96696a298f33724bf287a9d6b4d1b20a78a
clairtonluz <clairton.c.l@gmail.com>
2018-03-22 21:38:12 -0300

    correção de diferença de timezone

--- src/main/java/br/com/clairtonluz/sicoba/Application.java
+++ src/main/java/br/com/clairtonluz/sicoba/Application.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
     }
 }
