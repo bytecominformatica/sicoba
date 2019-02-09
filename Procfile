a049bdba603d0a6be7144d1d278667dcc50069ac
clairtonluz <clairton.c.l@gmail.com>
2019-02-09 10:05:35 -0300

    separando front e back

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/sicoba-2.0.0.jar
--- src/main/resources/public/app/controllers/estoque/equipamento/equipamento.list.controller.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('sicobaApp')
-        .controller('EquipamentoListCtrl', function ($scope, Equipamento) {
-            $scope.equipamentos = Equipamento.query();
-        });
-}());
